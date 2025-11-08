9923968f9152b2dd4ee1d6ac60cd29af712800ba
GiannisN5 <gtsades5@gmail.com>
2025-11-08 20:40:27 +0200

    Updated code and added security checks.

--- kernel/k22tree.c
+++ kernel/k22tree.c
@@ -13,14 +13,24 @@
 #include <linux/pid.h>
 
 #define task_next_child(p) \
-	list_first_entry(&p->children, struct task_struct, sibling)
+	list_first_entry_or_null(&p->children, struct task_struct, sibling)
+
+static void find_proc_and_set(struct task_struct *p, struct k22info *target, int size)
+{
+	for(int i = 0; i < size; ++i){
+		if (task_pid_nr(p) == target[i].pid) {
+			target[i].pid = -1;
+			break;
+		}
+	}
+}
 
 static void insert_proc(struct k22info *buf, struct task_struct *p, int i)
 {
 	snprintf(buf[i].comm, sizeof(buf[i].comm), "%s", p->comm);
 	buf[i].pid = task_pid_nr(p);
-	buf[i].parent_pid = task_pid_nr(p->parent);
-	buf[i].first_child_pid = task_pid_nr(task_next_child(p));
+	buf[i].parent_pid = task_pid_nr(p->real_parent);
+	buf[i].first_child_pid = task_next_child(p) ? task_pid_nr(task_next_child(p)) : 0;
 	buf[i].next_sibling_pid = task_pid_nr(list_next_entry(p, sibling));
 	buf[i].nvcsw = p->nvcsw;
 	buf[i].nivcsw = p->nivcsw;
@@ -44,7 +54,8 @@ static int do_k22tree(struct k22info *buf, int *ne)
 		return_value = -EINVAL;
 		goto out;
 	}
-	pnum = 0;
+	struct k22info *tmp_buff = kmalloc(420 * sizeof(*tmp_buff), GFP_KERNEL);
+	pnum = 1;
 	for_each_process(p)
 		++pnum;
 	if (copy_from_user(&upbound, ne, sizeof(int))) {//Needs fixing on i
@@ -52,7 +63,7 @@ static int do_k22tree(struct k22info *buf, int *ne)
 		goto out;
 	}
 	buffer_size = (pnum <= upbound) ? pnum : upbound;
-	proc_buf = kmalloc((buffer_size) * sizeof(*buf), GFP_KERNEL);
+	proc_buf = kmalloc((buffer_size) * sizeof(*proc_buf), GFP_KERNEL);
 	if (!proc_buf) {
 		return_value = -ENOMEM;
 		goto out;
@@ -63,14 +74,19 @@ static int do_k22tree(struct k22info *buf, int *ne)
 	dbg_count = 0;
 	read_lock(&tasklist_lock);
 	do {
-		pnum = 0;
-		for_each_process(p)
+		pnum = 1;
+		for_each_process(p){
+			insert_proc(tmp_buff, p, pnum);
+			pr_info("K22-TWOS |-- Name: %s, PID: %d, Next child PID: %d, Next sibling PID: %d, Parent PID: %d\n", p->comm, task_pid_nr(p), \
+			 task_next_child(p) ? task_pid_nr(task_next_child(p)) : 0, task_pid_nr(list_next_entry(p, sibling)), task_pid_nr(p->real_parent));
 			++pnum;
+		}
 		new_buffer_size = (pnum <= upbound) ? pnum : upbound;
 		if (new_buffer_size > buffer_size) {
 			read_unlock(&tasklist_lock);
 			buffer_size *= 2;
 			kfree(proc_buf);
+			pr_info("K22-TWOS | Updating buffer size to: %d from %d\n", buffer_size, buffer_size / 2);
 			proc_buf = kmalloc((buffer_size) * sizeof(*buf), GFP_KERNEL);
 			if (!proc_buf) {
 				return_value = -ENOMEM;
@@ -78,22 +94,24 @@ static int do_k22tree(struct k22info *buf, int *ne)
 			}
 			read_lock(&tasklist_lock);
 		} else {
+			pr_info("K22-TWOS | Buffer has the correct size. The upper limit is: %d and the buffer size: %d\n", upbound, buffer_size);
 			break;
 		}
 	} while (1);
 	do {
 		++dbg_count;
-		pr_info("K22-TWOS | Iter: %d, i: %d, PID: %d, Next child PID: %d, Next child prt:\
-			 %p, Next sibling PID: %d, Flag: %d\n", dbg_count, i, task_pid_nr(p), \
-			 task_pid_nr(task_next_child(p)), \
-			 (p->children).next, task_pid_nr(list_next_entry(p, sibling)), flag);
+		pr_info("K22-TWOS | Iter: %d, i: %d, Name: %s, PID: %d, Next child PID: %d, Next sibling PID: %d, Parent PID: %d, Flag: %d\n", dbg_count, i, p->comm, task_pid_nr(p), \
+			task_next_child(p) ? task_pid_nr(task_next_child(p)) : 0, task_pid_nr(list_next_entry(p, sibling)), task_pid_nr(p->real_parent),flag);
 		if (list_empty(&(p->children)) || (flag == 1)) {
-			if (flag != 1)
+			if (flag != 1) {
 				insert_proc(proc_buf, p, i++);
-			if ((p->sibling).next == &(p->parent->children)) {
+				find_proc_and_set(p, tmp_buff, pnum);
+				pr_info("K22-TWOS | Inserting process\n");
+			}
+			if ((p->sibling).next == &(p->real_parent->children)) {
 				pr_info("K22-TWOS | Flag set to 1. Visiting parent\n");
 				flag = 1;
-				p = p->parent;
+				p = p->real_parent;
 			} else {
 				pr_info("K22-TWOS | Visiting next sibling\n");
 				flag = 0;
@@ -101,12 +119,16 @@ static int do_k22tree(struct k22info *buf, int *ne)
 			}
 		} else {
 			insert_proc(proc_buf, p, i++);
+			find_proc_and_set(p, tmp_buff, pnum);
 			pr_info("K22-TWOS | Inserted process. Visiting next child\n");
 			p = task_next_child(p);
 		}
 		if (i == upbound)
 			break;
 	} while ((p != &init_task) || (flag != 1));
+
+	for(int k = 0; k < pnum; ++k)
+		pr_info("K22-TWOS | Name: %s, PID: %d, Next child PID: %d, Next sibling PID: %d, Parent PID: %d\n", tmp_buff[k].comm, tmp_buff[k].pid, tmp_buff[k].first_child_pid, tmp_buff[k].next_sibling_pid, tmp_buff[k].parent_pid);
 
 	read_unlock(&tasklist_lock);
 	if (copy_to_user(buf, proc_buf, i * sizeof(*proc_buf))) {//Needs fixing on i
