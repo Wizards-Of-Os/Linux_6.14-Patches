f03a39604365bb4ac3e06cbd67b174129f4d32fa
Karadimoss <thanasi291205@gmail.com>
2025-12-14 02:48:35 +0200

    Fixed some micro errors

--- include/asm-generic/vmlinux.lds.h
+++ include/asm-generic/vmlinux.lds.h
@@ -133,19 +133,33 @@ defined(CONFIG_AUTOFDO_CLANG) || defined(CONFIG_PROPELLER_CLANG)
  * used to determine the order of the priority of each sched class in
  * relation to each other.
  */
+
+#ifdef CONFIG_GRR_SCHED
+	
 #define SCHED_DATA				\
 	STRUCT_ALIGN();				\
 	__sched_class_highest = .;		\
 	*(__stop_sched_class)			\
 	*(__dl_sched_class)			\
 	*(__rt_sched_class)			\
-#ifdef CONFIG_GRR_SCHED
 	*(__grr_sched_class)			\
-#endif	
 	*(__fair_sched_class)			\
 	*(__ext_sched_class)			\
 	*(__idle_sched_class)			\
 	__sched_class_lowest = .;
+
+#else
+	#define SCHED_DATA				\
+		STRUCT_ALIGN();				\
+		__sched_class_highest = .;		\
+		*(__stop_sched_class)			\
+		*(__dl_sched_class)			\
+		*(__rt_sched_class)			\
+		*(__fair_sched_class)			\
+		*(__ext_sched_class)			\
+		*(__idle_sched_class)			\
+		__sched_class_lowest = .;
+#endif
 
 /* The actual configuration determine if the init/exit sections
  * are handled as text/data or they can be discarded (which
