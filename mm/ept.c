71013cd17983d90b8901dd6305a4eef22ff08ab5
Karounos <vankarounos@gmail.com>
2026-01-31 19:45:33 +0200

    Final commit!

--- mm/ept.c
+++ mm/ept.c
@@ -1,5 +1,8 @@
 // SPDX-License-Identifier: GPL-2.0-only
-
+/*
+ * =====    The Wizards of Os	  =====
+ *		=====   Abra Kadabra  =====
+ */
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
